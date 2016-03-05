cc2873d4ca4df0ec0feed736c65f743113a01dde
student <student@achiever96.local>
2016-03-05 20:37:11 +0530

    Initial Commit

---
+++ DatePickerPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DatePickerPractice
+//
+//  Created by student on 05/03/16.
+//  Copyright © 2016 BRN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DatePickerPractice/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DatePickerPractice
+//
+//  Created by student on 05/03/16.
+//  Copyright © 2016 BRN. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
